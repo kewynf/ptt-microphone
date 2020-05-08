a18238930021f37ff54c1402b8b47ecc812b9a7f
Kewyn Ferreira <47131740+kewynf@users.noreply.github.com>
2020-05-08 13:23:23 -0300

    V1.0 code
    Final tested version of this code.

--- PTT.ino
+++ PTT.ino
@@ -1,74 +1,103 @@
-void setup()
-{
-  #include <Keyboard.h>  // Includes the Keyboard library.
-  
-  pinMode(9, OUTPUT);    // Defines Pin 9 as output. PLUG THE RED PART OF THE LED IN THIS PIN.
-  pinMode(10, OUTPUT);   // Defines Pin 10 as output PLUG THE GREEN PART OF THE LED IN THIS PIN.
-  pinMode(11, OUTPUT);   // Defines Pin 11 as output PLUG THE BLUE PART OF THE LED IN THIS PIN.
+//Includes the Keyboard Library, so the device will be recognized as an HID.
+#include <Keyboard.h>
 
-  pinMode(18, INPUT);    // Defines Pin 18 as output
-  pinMode(19, INPUT);    // Defines Pin 18 as output
-  pinMode(20, INPUT);    // Defines Pin 18 as output
-  pinMode(21, INPUT);    // Defines Pin 18 as output
-  Serial.begin(9600);    // Starts Serial Port (to get debug information through IDE)
+//Defines where the LED is connected to the Arduino.
+int red = 3;
+int green = 6;
+int blue = 9;
+
+//Defines where the buttons are connected to the Arduino.
+int ptt = A0;
+int button1 = A1;
+int button2 = A2;
+int button3 = A3;
+
+void setup() {
+
+  //Starts Serial Communication
+  Serial.begin(9600);
+
+  //Tells the Arduino to use LED as output.
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+
+  //Tells the Arduino to use buttons as INPUT.
+  pinMode(ptt, INPUT);
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(button3, INPUT);
 }
 
-void loop()
-{
+void loop() {
   
-  //Keep in mind: this code was designed to work with common anode RGB LED. So, when you want some color ON, you turn the digital pins OFF.)
-  //Sets up LED full RED, to indicate when it is on)
-  digitalWrite(9, 0);
-  digitalWrite(10, 1);
-  digitalWrite(11, 1);
+  //Sets LED to show RED, indicating Standby.
+  analogWrite(red, 0);
+  analogWrite(green, 255);
+  analogWrite(blue, 255);
 
-  //Reads the main trigger status. If pressed, will give it a keyboard stroke.
-  if (digitalRead(9) == HIGH) {
-    //Sends keyboard command to print ")" if trigger is pressed.
-    Keyboard.write(41)
-    //Turns LED Green
-    analogWrite(9, 1);
-    analogWrite(10, 0);
-    analogWrite(11, 1);
-    //Sends information through Serial Monitor
-    Serial.println("PTT ON");
+  //Reads PTT trigger. If pressed, will execute action down the line.
+  if(digitalRead(ptt) == HIGH){
+    
+    Keyboard.press(KEY_LEFT_CTRL);  //Presses Left CTRL.
+    Serial.print("PTT");            //Sends information to Serial Monitor
+
+    //Turns LED green
+    analogWrite(red,255);
+    analogWrite(green,0);
+    analogWrite(blue,255);
+  }
+  //If PTT trigger is not pressed, will execute action down the line:
+  else{
+    Keyboard.release(KEY_LEFT_CTRL); //Release Left CTRL.
   }
 
-  //Reads the upper button status. If pressed, will give it a keyboard stroke.
-  if (digitalRead(9) == HIGH) {
-    //Sends keyboard command to print "*" if trigger is pressed.
-    Keyboard.write(42)
-    //Turns LED White
-    digitalWrite(9, 1);
-    digitalWrite(10, 1);
-    digitalWrite(11, 1);
-    //Sends information through Serial Monitor
-    Serial.println("BUTTON 1 ON");
+  //Reads Upper button. If pressed, will execute action down the line.
+  if(digitalRead(button1) == HIGH){
+    
+    Keyboard.press(KEY_RIGHT_CTRL);  //Presses Right CTRL.
+    Serial.print("BUTTON 1");            //Sends information to Serial Monitor
+
+    //Turns LED dimm white.
+    analogWrite(red,200);
+    analogWrite(green,200);
+    analogWrite(blue,200);
+  }
+  //If Upper Button is not pressed, will execute action down the line:
+  else{
+    Keyboard.release(KEY_RIGHT_CTRL); //Release Right CTRL.
   }
 
-  //Reads the middle button status. If pressed, will give it a keyboard stroke.
-  if (digitalRead(9) == HIGH) {
-    //Sends keyboard command to print "+" if trigger is pressed.
-    Keyboard.write(43)
-    //Turns LED Purple
-    digitalWrite(9, 0);
-    digitalWrite(10, 1);
-    digitalWrite(11, 0);
-    //Sends information through Serial Monitor
-    Serial.println("BUTTON 2 ON");
+    //Reads Middle Button. If pressed, will execute action down the line.
+  if(digitalRead(button2) == HIGH){
+    
+    Keyboard.press(KEY_HOME);  //Presses HOME.
+    Serial.print("BUTTON 2");            //Sends information to Serial Monitor
+
+    //Turns LED purple
+    analogWrite(red,0);
+    analogWrite(green,255);
+    analogWrite(blue,0);
+  }
+  //If Middle Button is not pressed, will execute action down the line:
+  else{
+    Keyboard.release(KEY_HOME); //Release HOME key.
   }
 
-  //Reads the lower button status. If pressed, will give it a keyboard stroke.
-  if (digitalRead(9) == HIGH) {
-    //Sends keyboard command to print "&" if trigger is pressed.
-    Keyboard.write(38)
-    //Turns LED Blue
-    digitalWrite(9, 0);
-    digitalWrite(10, 0);
-    digitalWrite(11, 1);
-    //Sends information through Serial Monitor
-    Serial.println("BUTTON 2 ON");
+    //Reads Bottom button. If pressed, will execute action down the line.
+  if(digitalRead(button3) == HIGH){
+    
+    Keyboard.press(KEY_END);  //Presses Left CTRL.
+    Serial.print("BUTTON 3");            //Sends information to Serial Monitor
+
+    //Turns LED dimm RED (it might seem like we've turned the LED black, but due to Arduino duty cicle, it remains slightly RED due to the standby red light
+    analogWrite(red,255);
+    analogWrite(green,255);
+    analogWrite(blue,255);
   }
-  
-  delay(10); //Delays the next execution, so the computer doesn't get overflown. Can (and should) be removed if yout machine can handle the pooling from Arduino.
-}
+  //If Bottom button is not pressed, will execute action down the line:
+  else{
+    Keyboard.release(KEY_END); //Release END key.
+  }
+  delay(30); //States a delay to not overpool the computer.
+  }
